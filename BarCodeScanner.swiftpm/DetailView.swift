7413c9d98217a97a72e8454841105b4f5c53f895
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-02-25 09:48:52 +0530

    add

---
+++ BarCodeScanner.swiftpm/DetailView.swift
@@ -0,0 +1,19 @@
+//
+//  DetailView.swift
+//  BarCodeScanner
+//
+//  Created by Aditya Gaba on 23/02/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+#Preview {
+    DetailView()
+}
